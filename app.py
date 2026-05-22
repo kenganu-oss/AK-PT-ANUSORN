9d0845604da69b962803eb5b8a0b22969df85660
kenganu-oss <keng.annii@gmail.com>
2026-05-22 15:52:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,5 @@ if st.button("💰 ระบบคำนวณส่วนลดตามยอ�
     st.switch_page("pages/app1_discount_calc.py")
 elif st.button("💰 การทำความสะอาดข้อมูล"):
     st.switch_page("pages/clean_app.py")
+elif st.button("💰 clean_customers"):
+    st.switch_page("clean_customers.py")
