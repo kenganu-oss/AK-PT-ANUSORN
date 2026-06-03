ca638996a47748d4b15612f910423a494a69cb46
kenganu-oss <keng.annii@gmail.com>
2026-06-03 15:27:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ elif st.button("💰 การทำความสะอาดข้อมู�
 elif st.button("💰 การแปลงข้อมูล"):
     st.switch_page("pages/transform_app.py")
 elif st.button("💰 การวิเคราะห์ข้อมูลเชิงสำรวจ"):
-    st.switch_page("pages/EDA__app.py")
+    st.switch_page("pages/EDA_app.py")
