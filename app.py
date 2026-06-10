3c55c5eb0fbdfac72cd9cc3dff14afb83a41dc8a
kenganu-oss <keng.annii@gmail.com>
2026-06-10 15:17:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,3 +27,5 @@ elif st.button("💰 การจัดกลุ่มข้อมูล"):
     st.switch_page("pages/clustering_segment.py") 
 elif st.button("💰 ระบบแนะนำสินค้าจาก Market Basket Analysis"):
     st.switch_page("pages/association_items.py") 
+elif st.button("💰 ระบบแนะนำสินค้า"):
+    st.switch_page("pages/association_recommend.py") 
